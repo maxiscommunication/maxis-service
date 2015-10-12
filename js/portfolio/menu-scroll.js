193622b2dad6d45b899917c9279fc4bcd487627e
tkmaaxl <Chermarajan.Arumugasamy@kohls.com>
2015-10-12 21:08:18 +0530

    Updated Active products menu

--- js/portfolio/menu-scroll.js
+++ js/portfolio/menu-scroll.js
@@ -1,9 +1,24 @@
 /**
- * Created by murali on 5/10/15.
+ * Created by murali on 12/10/15.
  */
-// Carousel Auto-Cycle
 $(document).ready(function() {
-    $('.carousel1').carousel({
+
+    var owl = $(".owl-comm");
+
+    owl.owlCarousel({
+        items :5, //10 items above 1000px browser width
+        itemsDesktop : [1000,5], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,5], // betweem 900px and 601px
+        itemsTablet: [600,2], //2 items between 600 and 0
+        itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
         interval: 60
+    });
+
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+        owl.trigger('owl.prev');
     })
 });
