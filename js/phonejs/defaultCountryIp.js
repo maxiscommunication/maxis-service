6c20b78d213a5e7069f3e352a28fabaca6b86233
tkmaaxl <Chermarajan.Arumugasamy@kohls.com>
2016-03-10 00:16:17 +0530

    contact us form completed partially 2[Enhancement 6]

---
+++ js/phonejs/defaultCountryIp.js
@@ -0,0 +1,14 @@
+$("#phone").intlTelInput({
+  initialCountry: "auto",
+  geoIpLookup: function(callback) {
+    $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+      var countryCode = (resp && resp.country) ? resp.country : "";
+      callback(countryCode);
+    });
+  },
+  utilsScript: "js/utils.js" // just for formatting/placeholders etc
+
+
+});
+
+
